dcf2115bfa3efc58a4efc215716a92bbadd314b2
Alec M <alecmattu1234@gmail.com>
2021-11-27 10:06:51 -0500

    Fix Array.map usage

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,25 +1,25 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { RiCloseCircleLine } from "react-icons/ri";
 import { TiEdit } from "react-icons/ti";
 
-function Todo({todos, completeTodo, removeTodo, updateTodo}) {
+const Todo = ({todos, completeTodo, removeTodo, updateTodo}) => {
   const [edit, setEdit] = useState({
     id: null,
     value: ''
   });
 
   return todos.map((todo, index) => {
-    <div
-      className={todo.isComplete ? "todo-row complete" : "todo-row"}
-      key={index}>
-      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-        {todo.text}
+    return (
+      <div key={index} className={todo.isComplete ? "todo-row complete" : "todo-row"}>
+        <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+          {todo.text}
+        </div>
+        <div className="icons">
+          <RiCloseCircleLine onClick={() => removeTodo(todo.id)} className="delete-icon" />
+          <TiEdit onClick={() => setEdit({id: todo.id, value: todo.text})} className="edit-icon" />
+        </div>
       </div>
-      <div className="icons">
-        <RiCloseCircleLine onClick={() => removeTodo(todo.id)} className="delete-icon" />
-        <TiEdit onClick={() => setEdit({id: todo.id, value: todo.text})} className="edit-icon" />
-      </div>
-    </div>
+    )
   });
 }
 
